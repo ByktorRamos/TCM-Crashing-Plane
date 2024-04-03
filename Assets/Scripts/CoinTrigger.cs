ba50d75dee246248664349732f87d9a31cbf273c
victorRamos <vramosp@edu.tecnocampus.cat>
2024-04-03 17:05:55 +0200

    Moneda y Collision

---
+++ Assets/Scripts/CoinTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTrigger : MonoBehaviour
+{
+    private int moneda;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            Sumarmoneda();
+
+
+        if (collision.tag == "Player")
+        {
+           // Sumarmoneda();
+            Destroy(this.gameObject);
+        }
+        
+
+    }
+
+    private void Sumarmoneda()
+    {
+        moneda++;
+        Debug.Log(moneda);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
